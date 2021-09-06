6e8378cad062a01e6adc60daf75d4dd5b9bab008
okanumayuta <mj.dokinchan@outlook.com>
2021-09-06 22:22:41 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+  def index
+
+    @prototypes = Prototype.includes(:user)
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    #binding.pry
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+ 
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+end
