82d64170dc4ccafd3ef54e7d9237e3e4ce6f87de
okanumayuta <mj.dokinchan@outlook.com>
2021-09-07 13:02:36 +0900

    issue修正2回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to  prototype_path(@comment.prototype_id)
     else
+      @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :new, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
   def index
 
     @prototypes = Prototype.includes(:user)
